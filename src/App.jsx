f7412be6eeeb2714732ba0866917d452a3ad84e3
Sadiq <seeker99157@gmail.com>
2023-05-29 16:06:56 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "idyllic-ui"
+
+const App = () => {
+  return (
+    <div>
+      <p>Buttons from library</p>
+      <Button>Default button</Button>
+      <Button variant="secondary">Secondary button</Button>
+    </div>
+  )
+}
+export default App
